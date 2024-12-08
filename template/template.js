4117e0cf075cef5ae3c4df6f7a520f755344b070
unknown <pkpc22042023@gmail.com>
2024-12-08 10:04:12 +0530

    dont remember what changes i made

--- template/template.js
+++ template/template.js
@@ -4,3 +4,10 @@ editor.session.setMode("ace/mode/c_cpp");
 editor.resize();
 editor.setTheme("ace/theme/cobalt"); 
 editor.setFontSize(19);
+
+
+var editor = ace.edit("editor2");
+editor.session.setMode("ace/mode/c_cpp");  
+editor.resize();
+editor.setTheme("ace/theme/cobalt"); 
+editor.setFontSize(19);
